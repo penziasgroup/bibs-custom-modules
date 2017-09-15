332341ea6ad19fe0df2c527021b0e94bac646f1b
rossetti <mike.rossetti@penzias.com>
2017-09-15 09:38:56 -0400

    map styles

--- campus_location/js/campus_location.js
+++ campus_location/js/campus_location.js
@@ -35,6 +35,46 @@
               ]
             },
             {
+              "featureType": "poi.government",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.medical",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.park",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.place_of_worship",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.school",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
               "featureType": "poi.sports_complex",
               "stylers": [
                 {
